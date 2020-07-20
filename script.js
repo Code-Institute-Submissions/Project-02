6b35eeaa97d98d3d42cc9de2c0409942e225a12d
darylkangjh <daryl.kangjh@gmail.com>
2020-07-20 02:43:05 +0000

    testing for l.marker

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 //client ID and the client Secret
 const CLIENT_ID = "KNFI1FV1IHR5VB0AS00HIYMECUTITHFHY1RKBNZDWUWCY0BY";
 const CLIENT_SECRET = "DYS3XTXRYTA3FAGNLFY03RAIETOVWMSSN5CV4CFRRRX44S1B";
+const baseURL = "https://api.foursquare.com/v2/"
 
 // state variable
-let foodLoaded = 0;
+// This is global state for the two location keyed queried in initially 
+let inputLocation = 0;
+// This is the amount of suggestions within a 5km radus
+let suggestions = 0;
+// Search origin before keying in inputs
 let searchOrigin = [1.370462, 103.810777]; 
 
 // Map display
@@ -12,17 +17,16 @@ let map = L.map("map").setView(singapore, 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 12,
+    maxZoom: 25,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 }).addTo(map)
 
-$(function(){
-    let firstPostalCode = $(#postal1);
-    let secondPostalCode = $(#postal2);
-})
 
-// let singaporeMarker = L.marker([1.29, 103.85])
-// singaporeMarker.addTo(map);
+
+// When user key in two params
+$("#generate").click(function() {
+    L.marker(singapore).addTo(map);
+    });
