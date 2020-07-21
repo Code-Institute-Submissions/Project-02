7a6eba2cc73cf4b6bfaae5e22b79bf01e94ffdb9
darylkangjh <daryl.kangjh@gmail.com>
2020-07-21 07:36:18 +0000

    Got array from FourSquare! HOORAY!

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //client ID and the client Secret
 const CLIENT_ID = "KNFI1FV1IHR5VB0AS00HIYMECUTITHFHY1RKBNZDWUWCY0BY";
 const CLIENT_SECRET = "DYS3XTXRYTA3FAGNLFY03RAIETOVWMSSN5CV4CFRRRX44S1B";
-const baseURL = "https://api.foursquare.com/v2/venues/VENUE_ID"
+const searchURL = "https://api.foursquare.com/v2/venues/search";
 
 // state variable
 // This is the amount of suggestions within a 5km radus
@@ -13,41 +13,28 @@ let searchOrigin = [1.370462, 103.810777];
 let singapore = [1.370462, 103.810777]; // Singapore latlng
 let map = L.map("map").setView(singapore, 13);
 
-
-
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 25,
+    maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
-}).addTo(map)
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+}).addTo(map);
 
-$(function() {
-    // Global input
-    let input1 = $("#postal1")
-    let input2 = $("#postal2")
-    let postalLayer = L.layerGroup();
-    map.addLayer(postalLayer);
-
-    // When user key in two params
-    $("#generate").click(function() {
-            let searchResult1 = {
-                params: {
-                client_id: CLIENT_ID,
-                client_secret: CLIENT_SECRET,
-                v: "20200716",
-                ll: location.join(),
-                query: input1.val(),
-                offset: 0,
-                limit: 1
-                }
-            }
+    $('#generate').click(function(){
     
-    axios.get(baseURL, searchResult1).then(function(response){
-          let marker = L.marker([location.lat, location.lng]);
-          marker.addTo(postalLayer);
-      })
-    });
+        let searchTerms = $("#postal1").val();
+
+        axios.get(searchURL,{
+        params : {
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET,
+            v:"20200721",
+            ll: singapore.join(","),
+            query: searchTerms,
+            offset: 0,
+            limit: 1,
+        }
+    })
 })
